3ed5c76f94745da786db7ccf397ff7dac73faa67
augustinahansen <77749527+augustinahansen@users.noreply.github.com>
2021-01-28 13:21:56 -0800

    First project

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,4 @@
+print("Coding is really hard for me")
+print("\nI don't understand most of it")
+print("but it's starting to get easier")
+print("and I really like everything I've have learned so far.")
