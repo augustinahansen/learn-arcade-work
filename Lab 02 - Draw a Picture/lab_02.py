44bcdedb54e0032427b5c661f4bb52d5ed100658
augustinahansen <77749527+augustinahansen@users.noreply.github.com>
2021-02-22 19:01:28 -0800

    City Street Drawing

--- Lab 02 - Draw a Picture/lab_02.py
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -1,14 +1,55 @@
-"""
-This is a sample program to show how to draw using the Python programming
-language and the Arcade library.
-"""
-
 # Import the "arcade" library
 import arcade
 
 # Open up a window.
 # From the "arcade" library, use a function called "open_window"
-# Set the dimensions (width and height)
 # Set the window title to "Drawing Example"
+# Set the dimensions (width and height)
+arcade.open_window(900, 600, "Drawing Example")
 
-arcade.open_window(600, 600, "Drawing Example")
+# Set the background color
+
+arcade.set_background_color(arcade.csscolor.SKY_BLUE)
+
+
+# Get ready to draw
+
+arcade.start_render()
+
+# Draw the sun
+arcade.draw_circle_filled(800, 530, 60, arcade.color.YELLOW)
+
+# Rays for left, right, down, and up
+arcade.draw_line(600, 530, 800, 530, arcade.color.YELLOW, 3)
+arcade.draw_line(900, 530, 800, 530, arcade.color.YELLOW, 3)
+arcade.draw_line(800, 350, 800, 500, arcade.color.YELLOW, 3)
+arcade.draw_line(800, 600, 800, 500, arcade.color.YELLOW, 3)
+
+# Diagonal Rays
+arcade.draw_line(700, 450, 800, 530, arcade.color.YELLOW, 3)
+arcade.draw_line(890, 590, 800, 530, arcade.color.YELLOW, 3)
+
+
+
+
+
+
+
+
+
+
+# Finish drawing
+
+arcade.finish_render()
+
+
+# Keep the window up until someone closes it.
+arcade.run()
+
+
+
+
+
+
+
+
