c0e54f309e8817d64fc66969e835f6153c010558
augustinahansen <77749527+augustinahansen@users.noreply.github.com>
2021-06-08 23:37:18 -0700

    Update part_12.py

--- Lab 12 - Final Lab/part_12.py
+++ Lab 12 - Final Lab/part_12.py
@@ -1,27 +1,54 @@
 import arcade
-arcade.open_window(2000, 2000, "Welcome to Fun with Flags!")
-arcade.set_background_color(arcade.csscolor.BLACK)
+
+# Open window, set background to black
+arcade.open_window(1000, 1000, "Welcome to Fun with Flags!")
+arcade.set_background_color(arcade.csscolor.PINK)
 arcade.start_render()
 
 
 def opening_statements():
+    # Opening statements to player
     arcade.draw_text("""Let's test your knowledge on the wide world of flags!
 The questions get progressivley harder, but you got this!
-Answer to the best of your ability and give full answers.
-Let the game begin!""", 500, 1400, arcade.color.WHITE, 15)
+Answer to the best of your ability.
+Let the game begin!""", 300, 900, arcade.color.BLACK, 15)
 
 
-def american_flag():
-    arcade.draw_lrtb_rectangle_filled(50, 500, 1300, 1100, arcade.color.WHITE)
-    for row in range(13):
-        for column in range(13):
-            x = 1290  # Instead of zero, calculate the proper x location using 'column'
-            y = 1280 # Instead of zero, calculate the proper y location using 'row'
-            arcade.draw_lrtb_rectangle_filled(50, 500, x, y, arcade.color.RED)
+def german_flag():
+    # Draw German flag
+    arcade.draw_text("1.", 40, 850, arcade.color.BLACK, 30)
+    arcade.draw_lrtb_rectangle_filled(50, 350, 850, 750, arcade.color.WHITE)
+    arcade.draw_lrtb_rectangle_filled(50, 350, 850, 817, arcade.color.BLACK)
+    arcade.draw_lrtb_rectangle_filled(50, 350, 817, 784, arcade.color.RED)
+    arcade.draw_lrtb_rectangle_filled(50, 350, 784, 750, arcade.color.YELLOW)
 
 def monaco_flag():
-    arcade.draw_lrtb_rectangle_filled(50, 500, 1000, 800, arcade.color.WHITE)
-    arcade.draw_lrtb_rectangle_filled(50, 500, 1000, 900, arcade.color.RED)
+    # Draw Monaco flag
+    arcade.draw_text("2.", 50, 700, arcade.color.BLACK, 30)
+    arcade.draw_lrtb_rectangle_filled(50, 350, 700, 600, arcade.color.WHITE)
+    arcade.draw_lrtb_rectangle_filled(50, 350, 700, 650, arcade.color.RED)
+
+def nigeria_flag():
+    # Draw Nigerian flag
+    arcade.draw_text("3.", 50, 550, arcade.color.BLACK, 30)
+    arcade.draw_lrtb_rectangle_filled(50, 350, 550, 450, arcade.color.WHITE)
+    arcade.draw_lrtb_rectangle_filled(50, 150, 550, 450, arcade.color.GREEN)
+    arcade.draw_lrtb_rectangle_filled(250, 350, 550, 450, arcade.color.GREEN)
+
+def lithuania_flag():
+    # Draw Lithuanian flag
+    arcade.draw_text("4.", 50, 400, arcade.color.BLACK, 30)
+    arcade.draw_lrtb_rectangle_filled(50, 350, 400, 300, arcade.color.WHITE)
+    arcade.draw_lrtb_rectangle_filled(50, 350, 400, 367, arcade.color.YELLOW)
+    arcade.draw_lrtb_rectangle_filled(50, 350, 367, 334, arcade.color.GREEN)
+    arcade.draw_lrtb_rectangle_filled(50, 350, 334, 300, arcade.color.RED)
+
+def luxembourg_flag():
+    # Draw Luxembourg flag
+    arcade.draw_text("5.", 50, 250, arcade.color.BLACK, 30)
+    arcade.draw_lrtb_rectangle_filled(50, 350, 250, 150, arcade.color. WHITE)
+    arcade.draw_lrtb_rectangle_filled(50, 350, 250, 217, arcade.color.RED)
+    arcade.draw_lrtb_rectangle_filled(50, 350, 184, 150, arcade.color.LIGHT_BLUE)
 
 
 # Defining players score
@@ -32,7 +59,7 @@ def check_players_guess(guess, answer):
     while guessing and attempt < 3:
         if guess.lower() == answer.lower():
             print("Correct")
-            score = score + 1/10 * 100
+            score = score + 1/5 * 100
             guessing = False
         else:
             if attempt < 2:
@@ -41,19 +68,55 @@ def check_players_guess(guess, answer):
     if attempt == 3:
         print("The Correct answer is ",answer )
 
-
-
-
-
-
-
-# Call all functions
+# Call functions
 opening_statements()
-american_flag()
+german_flag()
 monaco_flag()
-
-
-
+nigeria_flag()
+lithuania_flag()
+luxembourg_flag()
 
 arcade.finish_render()
 arcade.run()
+
+
+# Variables
+score = 0
+
+# Message to player
+for i in range(3):
+    print("Please,")
+print("Answer in full names, not abbreviations.")
+
+# Question 1
+guess1 = input("Let's start off easy. What country does flag #1 represent?")
+guess1 = guess1.lstrip()
+check_players_guess(guess1, "Germany")
+
+
+# Question 2
+guess2 = input("This one is harder. What principality does flag #2 represent?")
+guess2 = guess2.lstrip()
+check_players_guess(guess2, "Monaco")
+
+
+# Question 3
+guess3 = input("Alright, here comes the challenge. What country does flag #3 represent?")
+guess3 = guess3.lstrip()
+check_players_guess(guess3, "Nigeria")
+
+
+# Question 4
+guess4 = input("And what country does flag #4 belong to?")
+guess4 = guess4.lstrip()
+check_players_guess(guess4, "Lithuania")
+
+
+# Questions 5
+guess5 = input("Last but not least, the hardest of them all. What country does flag #5 represent?")
+guess5 = guess5.lstrip()
+check_players_guess(guess5, "Luxembourg")
+
+
+# Ending message to player with their score
+print("Congratulations, your score is " + str(score))
